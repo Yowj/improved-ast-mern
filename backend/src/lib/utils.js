7a75b4a3cb168f1de67e40bfb405beba825cc79e
Yowj <eugenecoderx@gmail.com>
2025-05-14 04:56:45 +0800

    changed env

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,7 +8,7 @@ export const generateToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "Strict",
+    sameSite: "None",
   });
   return token;
 };
